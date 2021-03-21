c4392977fb75b7f264e9f2d33235afa10c69d63f
brunohendias <brunodiashen@gmail.com>
2021-03-20 21:25:15 -0300

    teste

---
+++ app/storage/framework/views/47d6b68278c2a97e308564cc1bd154be7bccb57a.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+	<App></App>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/resources/views/App.blade.php ENDPATH**/ ?>
---
+++ app/storage/framework/views/7bdeb8b0c526bafc45b4cf9183b44ebc3b0f0478.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+    <link rel="icon" href="/favicon.ico">
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('/js/app.js')); ?>" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <!-- Fonte awasome Icones-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('/css/app.css')); ?>" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <main>
+            <?php echo $__env->yieldContent('content'); ?>
+        </main>
+    </div>
+</body>
+</html><?php /**PATH /var/www/resources/views/layouts/app.blade.php ENDPATH**/ ?>

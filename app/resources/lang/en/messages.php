ca129ee4d3e217413b33abef1c89ec119b450634
brunohendias <brunodiashen@gmail.com>
2021-03-21 20:52:55 -0300

    messages.php, style

--- app/resources/lang/en/messages.php
+++ app/resources/lang/en/messages.php
@@ -8,13 +8,20 @@ return [
     |                   Messages
     |-------------------------------------------------------   
     |
-    | Modelo de mensagens para utilizar no retorno das rotas
+    |   Padrões de mensagem para resposta
     |
     */
-
+    'success' => [
+        'search' => 'Buscamos :value com sucesso.',
+        'register' => 'Cadastramos :value com sucesso.',
+        'edit' => 'Editamos :value com sucesso.',
+        'delete' => 'Deletamos :value com sucesso.'
+    ],
+    'error' => [
+        'search' => 'Houve um erro ao buscarmos :value.',
+        'register' => 'Houve um erro ao cadastrarmos :value.',
+        'edit' => 'Houve um erro ao editarmos :value.',
+        'delete' => 'Houve um erro ao deletarmos :value.'
+    ],
     'notfound' => 'Não encontramos nenhum(a) :value.',
-    'error' => 'Houve um erro ao :value',
-    'Searchsuccess' => 'Buscamos as :value com sucesso.',
-    'Editsuccess' => 'Editamos :value com sucesso.',
-    'Deletesuccess' => 'Deletamos :value com sucesso.',
 ];

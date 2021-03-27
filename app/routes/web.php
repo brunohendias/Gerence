ff729a164f8c6f71cd95e4f39bc597842e680704
brunodiashen <brunodiashen@gmail.com>
2021-03-27 14:36:40 -0300

    rotas midlleware, table user

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,56 +2,6 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-Route::prefix('/api')->group(function () {
-	Route::prefix('/v1')->group(function () {
-		Route::prefix('/series')->group(function() {
-			Route::get('/', 'SerieController@index');
-			Route::post('/', 'SerieController@store');
-		});
-		Route::get('/turnos', 'TurnoController@index');
-		Route::get('/turmas', 'TurmaController@index');
-		Route::get('/professores', 'ProfessorController@index');
-		Route::prefix('atencoes')->group(function () {
-			Route::get('/', 'AtencaoController@index');
-		});
-		Route::get('/situacoes', 'SituacaoController@index');
-		Route::prefix('inscricao')->group(function () {
-			Route::post('/busca', 'InscricaoController@index');
-		});
-		Route::prefix('candidato')->group(function () {
-			Route::post('/busca', 'CandidatoController@index');
-			Route::post('/', 'CandidatoController@store');
-			Route::delete('/{cod_can}', 'CandidatoController@destroy');
-		});
-		Route::prefix('/aluno')->group(function() {
-			Route::post('/busca', 'AlunoController@index');
-			Route::post('/', 'AlunoController@store');
-		});
-		Route::prefix('/serievinculo')->group(function() {
-			Route::post('/busca', 'SerieVinculoController@index');
-			Route::post('/', 'SerieVinculoController@store');
-			Route::put('/{cod_serie_v}', 'SerieVinculoController@update');
-		});
-		Route::post('/seriedisciplina/busca', 'SerieDisciplinaController@index');
-		Route::prefix('/professorvinculo')->group(function() {
-			Route::get('/', 'ProfessorVinculoController@index');
-		});
-		Route::prefix('relatorio')->group(function() {
-			Route::get('/atencoes', 'RelatorioController@atencoes');
-			Route::get('/inscricoes', 'RelatorioController@inscricoes');
-			Route::get('/infoseries', 'RelatorioController@infoSeries');
-		});
-	});
-});
-
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::view('/', 'App');
-Route::view('/{any}', 'App');
-Route::view('/{any}/{sub}', 'App');
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -62,3 +12,51 @@ Route::view('/{any}/{sub}', 'App');
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+Route::middleware('auth')->group(function () {	
+	Route::prefix('/api')->group(function () {
+		Route::prefix('/v1')->group(function () {
+			Route::prefix('/series')->group(function() {
+				Route::get('/', 'SerieController@index');
+				Route::post('/', 'SerieController@store');
+			});
+			Route::get('/turnos', 'TurnoController@index');
+			Route::get('/turmas', 'TurmaController@index');
+			Route::get('/professores', 'ProfessorController@index');
+			Route::prefix('atencoes')->group(function () {
+				Route::get('/', 'AtencaoController@index');
+			});
+			Route::get('/situacoes', 'SituacaoController@index');
+			Route::prefix('inscricao')->group(function () {
+				Route::post('/busca', 'InscricaoController@index');
+			});
+			Route::prefix('candidato')->group(function () {
+				Route::post('/busca', 'CandidatoController@index');
+				Route::post('/', 'CandidatoController@store');
+				Route::delete('/{cod_can}', 'CandidatoController@destroy');
+			});
+			Route::prefix('/aluno')->group(function() {
+				Route::post('/busca', 'AlunoController@index');
+				Route::post('/', 'AlunoController@store');
+			});
+			Route::prefix('/serievinculo')->group(function() {
+				Route::post('/busca', 'SerieVinculoController@index');
+				Route::post('/', 'SerieVinculoController@store');
+				Route::put('/{cod_serie_v}', 'SerieVinculoController@update');
+			});
+			Route::post('/seriedisciplina/busca', 'SerieDisciplinaController@index');
+			Route::prefix('/professorvinculo')->group(function() {
+				Route::get('/', 'ProfessorVinculoController@index');
+			});
+			Route::prefix('relatorio')->group(function() {
+				Route::get('/atencoes', 'RelatorioController@atencoes');
+				Route::get('/inscricoes', 'RelatorioController@inscricoes');
+				Route::get('/infoseries', 'RelatorioController@infoSeries');
+			});
+		});
+	});
+	Route::view('/', 'App');
+	Route::view('/{any}', 'App');
+	Route::view('/{any}/{sub}', 'App');
+});
